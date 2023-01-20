b6b1309dd8c98c8543d6414f5e3c11564f664a6f
CT-MarkLin <52391225+CT-MarkLin@users.noreply.github.com>
2023-01-20 01:25:15 +0000

    test

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const exec = require('child_process').execSync;
 // const process = require("child_process");
 
-// 执行 npm run build 命令
-;(function() {
-  exec('pm2 start ./amd64/hbbs && pm2 start ./amd64/hbbr', (error, stdout, stderr) => {
-    if (!error) {
-      // 成功
-      console.log("success___________________")
-    } else {
-      // 失败
-      console.log("fail___________________")
-    }
-  });
-})();
+// // 执行 npm run build 命令
+// ;(function() {
+//   exec('pm2 start ./amd64/hbbs && pm2 start ./amd64/hbbr', (error, stdout, stderr) => {
+//     if (!error) {
+//       // 成功
+//       console.log("success___________________")
+//     } else {
+//       // 失败
+//       console.log("fail___________________")
+//     }
+//   });
+// })();
 
 var str = exec('pwd');
 console.log(str.toString("utf8").trim());
